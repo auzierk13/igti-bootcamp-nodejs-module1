b5a3ca66c2fce27258c20e2b41c4287d9ad6d4ce
Bruno Auzier Cavalcante <auzierk13@gmail.com>
2023-04-07 17:25:57 -0400

    Import example

--- file_system/index.js
+++ file_system/index.js
@@ -1,6 +1,6 @@
-import {handler_file_1} from "./handler_file.js"
-import {handler_file_2} from "./handler_file_promises.js"
-import {handler_file_3} from "./handler_file_promises_async.js"
+import {handler_file_1} from "./handler_file_call_back.js"
+import {handler_file_2} from "./handler_file_then.js"
+import {handler_file_3} from "./handler_file_async.js"
 
 handler_file_1();
 handler_file_2();
